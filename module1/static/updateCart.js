e8e60d5d045b49267ed6d396228a63d3f5be332a
janki.p <janki9012@gmail.com>
2024-09-13 12:35:00 +0530

    add_product commit

--- module1/static/updateCart.js
+++ module1/static/updateCart.js
@@ -28,3 +28,36 @@ function removeProduct(cartId) {
             console.error('Error:', error);
         });
     }
+
+function addProduct(cartId) {
+        const qty = document.getElementById(`qty-${cartId}`).value;
+        const data = {
+            'cart_id': cartId,
+            'qty': qty
+        };
+
+        fetch('http://127.0.0.1:6002/addCartProduct', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (response.ok) {
+                 window.location.href = 'http://127.0.0.1:6001/show_cart';
+//                // Remove the product row from the table on success
+//                const row = document.getElementById(`product-row-${cartId}`);
+//                if (row) {
+//                    row.remove();
+//                }
+            } else {
+                console.error('Failed to add product');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+
+
